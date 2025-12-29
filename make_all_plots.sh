6fbad94e3e11ac48d35bf429e29240c33678af77
david <d.siska@ed.ac.uk>
2025-12-29 08:02:42 +0000

    plots

--- make_all_plots.sh
+++ make_all_plots.sh
@@ -21,7 +21,7 @@ python plots.py --figsize 6 3 --envname robotank --outfile ppo_vs_frppo_robotank
 # Individual plots Mujoco
 python plots.py --figsize 6 3 --envname walker2d --outfile ppo_vs_frppo_walker2d.pdf --patterns PPO_0 PPO_1 FRPPO_0 FRPPO_1  
 
-python plots.py --figsize 6 3 --envname hopper --outfile ppo_vs_frppo_hopper.pdf --patterns PPO_0 PPO_1 FRPPO_0  FRPPO_2 
+python plots.py --figsize 6 3 --envname hopper --outfile ppo_vs_frppo_hopper.pdf --patterns PPO_0 PPO_1 FRPPO_0 FRPPO_1 
 
 python plots.py --figsize 6 3 --envname reacher --outfile ppo_vs_frppo_reacher.pdf --patterns PPO_0 PPO_1 FRPPO_0 FRPPO_1 
 
@@ -32,6 +32,6 @@ python plots.py --figsize 6 3 --envname swimmer --outfile ppo_vs_frppo_swimmer.p
 python plots.py --figsize 6 3 --envname halfcheetah --outfile ppo_vs_frppo_halfcheetah.pdf --patterns PPO_0 PPO_1 FRPPO_0 FRPPO_1 
 
 # Grouped plots
-python ./multi_plots.py --envnames breakout beamrider kungfumaster mspacman pong robotank --outfile ppo_vs_frppo_all_atari.pdf --figsize 11 5
+python ./multi_plots.py --envnames breakout beamrider kungfumaster mspacman pong spaceinvaders --outfile ppo_vs_frppo_all_atari.pdf --figsize 11 5
 
 python ./multi_plots.py --envnames invertedpendulum reacher hopper walker2d swimmer halfcheetah --outfile ppo_vs_frppo_all_mujoco.pdf --figsize 11 5
